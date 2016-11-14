daf68cf57b9d1b51cf0af05a3a17b6d4158d1b81
Maurits van Rees <maurits@vanrees.org>
2016-11-14 12:45:32 +0100

    Back to development: 1.2.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='experimental.nodtml',
-    version='1.2',
+    version='1.2.1.dev0',
     description="Patch DocumentTemplate to not return any content.",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
